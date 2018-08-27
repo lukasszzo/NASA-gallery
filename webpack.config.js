ca4825da29a7b3b62741e8e5d1a7291f746ae67c
Łukasz Ustach <lukasz.ustach@gmail.com>
2018-08-27 10:05:23 +0200

    update version

--- webpack.config.js
+++ webpack.config.js
@@ -2,26 +2,26 @@ const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = {
     entry: './src/js/app.js',
-    output: {
+    output: { 
         path: `${__dirname}/dist/js`,
-        filename: 'script.min.js'
+        filename: 'script.min.js' 
     },
-    watch: false,
+    watch: false, //mozna pominac
     devtool: "source-map",
     module: {
         loaders: [
             {
-                test: /\.js$/,
+                test: /\.js$/,  
                 exclude: /(node_modules|bower_components)/,
                 use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['es2015']
+                    loader: 'babel',
+                    options: { 
+                        presets: ['es2015'] 
                     }
                 }
-
+                
             }
-        ],
+        ],        
     },
     plugins: [
         new UglifyJSPlugin({
